3e9f562c02e818aae3143ef0bf975a5b248bb1a5
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-11-28 17:58:04 -0700

    feat: proper public Load function through viper

--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+func Load() {
+  _, err := os.Stat("local.config.yaml")
+  if err != nil {
+    panic(fmt.Errorf("failed to locate local.config.yaml file", err))
+  }
+	viper.SetConfigName("local.config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+  err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+  fmt.Println("loaded 'local.config.yaml' OK")
+
+  if (viper.GetBool("debug.enabled")) {
+    fmt.Println("debug mode: enabled")
+    fmt.Println(viper.AllSettings())
+  }
+}
+
+type Config struct {
+
+}
